cc8a9efb93b2d4fb55229e7aadebab582a01a552
BagRoman01 <bagro02@mail.ru>
2024-08-12 13:05:20 +0300

    [FIX] docker fix

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from app.api.endpoints.authentication import auth
 from app.api.endpoints.currency import currency
+import uvicorn
 
 app = FastAPI()
 
 app.include_router(auth)
 app.include_router(currency)
 
-# if __name__ == '__main__':
-#     uvicorn.run(app, host='127.0.0.1', port=8080)
-#
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8080)
+
