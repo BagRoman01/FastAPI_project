4b3f70ed0c3a971a107e0e4792d8d4686b8c56d2
BagRoman01 <bagro02@mail.ru>
2024-08-22 18:35:07 +0300

    [ADD] кеширование с редис(пока без докера)

--- app/api/endpoints/currency.py
+++ app/api/endpoints/currency.py
@@ -2,20 +2,24 @@ from fastapi import APIRouter, Depends
 from app.api.dependencies import currency_service_dep
 from app.api.endpoints.authentication import authorize
 from app.api.schemas.currency import ExchangeCurrency, HistoryExchangeCurrency
+from fastapi_cache.decorator import cache
 
 currency = APIRouter(prefix='/currency', tags=['currency'])
 
 
 @currency.get('/currencies')
+@cache(expire=20)
 async def get_currencies(
         cur_service: currency_service_dep,
         user: str = Depends(authorize),
         currencies: str = None
 ):
+    print('Вывожу валюты.....')
     return await cur_service.get_currencies(currencies)
 
 
 @currency.post('/exchange')
+@cache(expire=5)
 async def exchange_currency(
         cur_service: currency_service_dep,
         exchange: ExchangeCurrency,
@@ -25,6 +29,7 @@ async def exchange_currency(
 
 
 @currency.post('/history_exchange')
+@cache(expire=5)
 async def exchange_currency(
         cur_service: currency_service_dep,
         exchange: HistoryExchangeCurrency,
--- app/main.py
+++ app/main.py
@@ -1,12 +1,36 @@
 from fastapi import FastAPI
 from app.api.endpoints.authentication import auth
 from app.api.endpoints.currency import currency
+from redis import asyncio as aioredis
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
 import uvicorn
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(this_app: FastAPI):
+    redis = aioredis.from_url("redis://localhost", encoding="utf-8", decode_responses=True)
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
+    pong = await redis.ping()
+    print(f"Connected to Redis: {pong}")
+    cache_backend = FastAPICache.get_backend()
+    print(f"Cache Backend Initialized: {cache_backend}")
+    # Check cache keys
+    all_keys = await redis.keys('*')
+    print(f"Current Redis Keys: {all_keys}")
+
+    yield
+
+    print('Clear cache!')
+    await FastAPICache.clear()
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(auth)
 app.include_router(currency)
+
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8080)
